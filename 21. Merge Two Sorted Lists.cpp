abccb5b2742de00406ef1448cddb23784d37d108
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-06 19:07:49 +0530

    ,

---
+++ 21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       ListNode* dummy = new ListNode(-1);
+       ListNode* curr = dummy;
+       while(list1!=NULL && list2!=NULL){
+           if(list1->val <= list2->val){
+               curr->next = list1;
+               list1=list1->next;
+           }
+           else{
+               curr->next = list2;
+               list2=list2->next;
+           }
+           curr=curr->next;
+       }
+       curr->next = list1!=NULL ? list1 : list2;
+       return dummy->next;
+    }
+};
