1eed1597cb7ed75bd038d152c9848081d2713368
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-07-10 12:29:46 +0530

    ,

---
+++ 1464. Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int res;
+        for(int i=0;i<n;i++){
+            res = (nums[n-1]-1) * (nums[n-2]-1);
+        }
+        return res;
+    }
+};
